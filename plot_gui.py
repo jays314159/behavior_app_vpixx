987cc028c49f34e393129e35f3179707fc0590b7
jays314159 <jay.s.314159@gmail.com>
2025-01-14 16:53:22 -0500

    Update plot_gui.py

--- plot_gui.py
+++ plot_gui.py
@@ -177,7 +177,7 @@ class PlotGui(FsmGui):
     @pyqtSlot()
     def toolbar_stop_QAction_triggered(self):
         #stop open Ephys
-        if self.open_ephys_started:
+        if self.open_ephys_connected:
             try:
                 open_ephys_msg = 'StopRecord'
                 self.open_ephys_socket.send_string(open_ephys_msg)
@@ -326,7 +326,7 @@ class PlotGui(FsmGui):
                     self.toolbar_stop_QAction.setDisabled(True)
 
                     #stop open Ephys
-                    if self.open_ephys_started:
+                    if self.open_ephys_connected:
                         try:
                             open_ephys_msg = 'StopRecord'
                             self.open_ephys_socket.send_string(open_ephys_msg)
