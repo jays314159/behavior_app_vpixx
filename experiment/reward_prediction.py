eb236581a550aa2b868c6d3e92f9ec8698fbfb5f
jays314159 <jay.s.314159@gmail.com>
2025-01-15 16:19:11 -0500

    Update reward_prediction.py

--- experiment/reward_prediction.py
+++ experiment/reward_prediction.py
@@ -816,7 +816,7 @@ class RwdPredFsmProcess(multiprocessing.Process):
     def update_target(self):
         tgt_parameter, _ = lib.load_parameter('','tgt_parameter.json',True,False,lib.set_default_tgt_parameter,'tgt')
         pd_tgt_parameter,_ = lib.load_parameter('','tgt_parameter.json',True,False,lib.set_default_tgt_parameter,'pd_tgt')
-        # fsm_parameter, _ = lib.load_parameter('experiment','exp_parameter.json',True,True,self.set_default_parameter,self.exp_name,self.main_parameter['current_monkey'])
+        fsm_parameter, _ = lib.load_parameter('experiment','exp_parameter.json',True,True,RwdPredGui.set_default_parameter,self.exp_name,self.main_parameter['current_monkey'])
         self.tgt = visual.Rect(win=self.window, width=tgt_parameter['size'],height=tgt_parameter['size'], units='deg', 
                       lineColor=tgt_parameter['line_color'],fillColor=tgt_parameter['fill_color'],
                       lineWidth=tgt_parameter['line_width'])
@@ -829,20 +829,20 @@ class RwdPredFsmProcess(multiprocessing.Process):
         self.pd_tgt.draw()
         self.rew_tgt = {}
         for counter_rew_tgt in range(self.num_rew_tgt):
-            # self.rew_tgt['h100'+str(counter_rew_tgt)] = visual.ImageStim(win=self.window, image=os.path.join(str(Path().absolute()),'icon','fractals','h100'+str(counter_rew_tgt)+'.png'),
-            #                 size=(fsm_parameter['rew_tgt_size'],fsm_parameter['rew_tgt_size']))
-            # self.rew_tgt['h100'+str(counter_rew_tgt)].draw()
-            # self.rew_tgt['l100'+str(counter_rew_tgt)] = visual.ImageStim(win=self.window, image=os.path.join(str(Path().absolute()),'icon','fractals','l100'+str(counter_rew_tgt)+'.png'),
-            #                 size=(fsm_parameter['rew_tgt_size'],fsm_parameter['rew_tgt_size']))
-            # self.rew_tgt['l100'+str(counter_rew_tgt)].draw()
-
-
             self.rew_tgt['h100'+str(counter_rew_tgt)] = visual.ImageStim(win=self.window, image=os.path.join(str(Path().absolute()),'icon','fractals','h100'+str(counter_rew_tgt)+'.png'),
-                            size=(2.0, 2.0))
+                            size=(fsm_parameter['rew_tgt_size'],fsm_parameter['rew_tgt_size']))
             self.rew_tgt['h100'+str(counter_rew_tgt)].draw()
             self.rew_tgt['l100'+str(counter_rew_tgt)] = visual.ImageStim(win=self.window, image=os.path.join(str(Path().absolute()),'icon','fractals','l100'+str(counter_rew_tgt)+'.png'),
-                            size=(2.0,2.0))
+                            size=(fsm_parameter['rew_tgt_size'],fsm_parameter['rew_tgt_size']))
             self.rew_tgt['l100'+str(counter_rew_tgt)].draw()
+
+
+            # self.rew_tgt['h100'+str(counter_rew_tgt)] = visual.ImageStim(win=self.window, image=os.path.join(str(Path().absolute()),'icon','fractals','h100'+str(counter_rew_tgt)+'.png'),
+            #                 size=(2.0, 2.0))
+            # self.rew_tgt['h100'+str(counter_rew_tgt)].draw()
+            # self.rew_tgt['l100'+str(counter_rew_tgt)] = visual.ImageStim(win=self.window, image=os.path.join(str(Path().absolute()),'icon','fractals','l100'+str(counter_rew_tgt)+'.png'),
+            #                 size=(2.0,2.0))
+            # self.rew_tgt['l100'+str(counter_rew_tgt)].draw()
 
         self.window.clearBuffer() # clear the back buffer of previously drawn stimuli - Poth, 2018
         
