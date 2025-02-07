ef44150f4a7582bf79e835fba9d878c3496a73ea
jays314159 <jay.s.314159@gmail.com>
2025-02-07 14:55:29 -0500

    Update tube_move.py

--- experiment/tube_move.py
+++ experiment/tube_move.py
@@ -110,7 +110,7 @@ class TubeMoveFsmProcess(multiprocessing.Process):
 
         run_exp = False
         random_signal_t = math.nan
-        bit_mask = 1 << 1 | 1 << 3
+        bit_mask = 1 << 8 | 1 << 10
         # Process loop
         while not self.stop_fsm_process_event.is_set():
             if not self.stop_exp_event.is_set():
@@ -134,11 +134,11 @@ class TubeMoveFsmProcess(multiprocessing.Process):
                 run_exp = True
 
             if self.tube_move_left_Event.is_set():
-                DPxSetDoutValue(1 << 1, bit_mask)
+                DPxSetDoutValue(1 << 8, bit_mask)
             elif self.tube_move_center_Event.is_set():
                 DPxSetDoutValue(0, bit_mask)
             elif self.tube_move_right_Event.is_set():
-                DPxSetDoutValue(1 << 3, bit_mask)
+                DPxSetDoutValue(1 << 10, bit_mask)
             DPxUpdateRegCache()
 
             # Trial loop
